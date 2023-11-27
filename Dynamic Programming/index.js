0246217c134020dcc94a895c3db1b0ea496c7791
Gaurav Deshwal <gauravd@geekyants.com>
2023-11-27 18:13:26 +0530

    Topics

---
+++ Dynamic Programming/index.js
@@ -0,0 +1,27 @@
+// Dynamic Programming:
+
+// Optimal Substructure and Overlapping Subproblems:
+// - Problems that can be broken down into smaller overlapping subproblems with optimal solutions.
+
+// Memoization and Tabulation:
+// - Techniques to store and reuse solutions of subproblems, either through caching or building a table.
+
+// Steps to Solve Problems using DP:
+// 1. Identify Overlapping Subproblems.
+// 2. Formulate a Recurrence Relation.
+// 3. Implement Memoization or Tabulation.
+// 4. Build Solution Bottom-Up or Top-Down.
+
+// Common Problems Solved Using DP:
+// - Fibonacci Series: Efficient computation using memoization or tabulation.
+// - Longest Common Subsequence (LCS): Finding the longest common subsequence.
+// - Knapsack Problem: Maximizing total value within a knapsack's capacity.
+// - Shortest/Longest Path Problems: Finding optimal paths in graphs.
+// - Edit Distance: Minimum operations to convert one string to another.
+
+// Benefits of Dynamic Programming:
+// - Efficiency Improvement: Avoiding redundant calculations.
+// - Optimal Solutions: Systematically evaluating all subproblems for best solutions.
+// - Applicability: Wide application across various problem domains.
+
+// Dynamic programming is a powerful algorithmic technique utilized across multiple disciplines, offering efficient solutions to problems with optimal substructure and overlapping subproblems.
---
+++ Javascript/Advanced Topics/React topics.js
@@ -0,0 +1,24 @@
+//* React Advanced Topics:
+
+// - Virtual DOM
+// - React Fiber Architecture
+// - Reconciliation Algorithm
+// - React Lifecycle
+// - State Management Behind the Scenes
+// - Component Rendering in React
+// - Props Drilling
+// - Components and Props
+// - State and Lifecycle Management
+// - React Hooks
+// - React Router
+// - Forms and Events Handling
+// - Conditional Rendering
+// - Lists and Keys in React
+// - Styling in React
+// - Context API Usage
+// - Higher-Order Components (HOCs)
+// - Render Props Pattern
+// - Best Practices with React Hooks
+// - React Testing (React Testing Library)
+// - Performance Optimization in React
+// - Lazy Loading and Suspense in React
