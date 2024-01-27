7d8ccfbe1cdf2d1e5f0125dc8a09b82f4df5a544
Gaurav Deshwal <gauravd@geekyants.com>
2024-01-27 21:56:13 +0530

    DSA Changes

--- DSA/Problems/Data structures/DP/Question 9/index.js
+++ DSA/Problems/Data structures/DP/Question 9/index.js
@@ -12,7 +12,7 @@ var canJump = function (nums) {
   let n = nums.length;
   let goal = n - 1;
 
-  for (let i = n - 1; i >= 0; i--) {
+  for (let i = n - 2; i >= 0; i--) {
     if (i + nums[i] >= goal) {
       goal = i;
     }
