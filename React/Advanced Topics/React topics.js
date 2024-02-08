8f7451c8bae1e89993aa5b546a4017dcdd6a77b5
Gaurav Deshwal <gauravd@geekyants.com>
2024-02-08 15:50:56 +0530

    React Advanced Topics

--- React/Advanced Topics/React topics.js
+++ React/Advanced Topics/React topics.js
@@ -11,3 +11,11 @@
 // - Render Props Pattern
 // - Performance Optimization in React
 // - Lazy Loading and Suspense in React
+
+//* Experience working with WebSockets, RTC with React JS
+
+//* How will you architecture a large scale react application for better performace
+
+//* How will you automate ened to end testing in  react application
+
+//* How will you migrate css of entriprese level project to avoid any issues and conflicts?
