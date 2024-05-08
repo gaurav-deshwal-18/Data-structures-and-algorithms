1524dc8fddac6d63926454452e4fa9cf292c8d2f
Gaurav Deshwal <gauravd@geekyants.com>
2024-05-08 10:59:04 +0530

    DSA

--- DSA/Problems/Data structures/Trie/Medium/Question 4/index.js
+++ DSA/Problems/Data structures/Trie/Medium/Question 4/index.js
@@ -1,46 +1,30 @@
 //* 	Extra Characters in a String
+
+//* DP Solution
+
 var minExtraChar = function (s, dictionary) {
-  class TrieNode {
-    constructor() {
-      this.children = {};
-      this.isWord = false;
+  const words = new Set(dictionary);
+
+  const dp = {};
+
+  function dfs(i) {
+    if (i === s.length) {
+      return 0;
     }
+    if (i in dp) {
+      return dp[i];
+    }
+
+    let res = 1 + dfs(i + 1); // skip current char
+
+    for (let j = i; j < s.length; j++) {
+      if (words.has(s.substring(i, j + 1))) {
+        res = Math.min(res, dfs(j + 1));
+      }
+    }
+    dp[i] = res;
+    return res;
   }
 
-  const buildTrie = function (dictionary) {
-    const root = new TrieNode();
-    for (const word of dictionary) {
-      let node = root;
-      for (const ch of word) {
-        if (!(ch in node.children)) {
-          node.children[ch] = new TrieNode();
-        }
-        node = node.children[ch];
-      }
-      node.isWord = true;
-    }
-    return root;
-  };
-
-  const root = buildTrie(dictionary);
-  const n = s.length;
-  const dp = new Array(n + 1).fill(Number.POSITIVE_INFINITY);
-  dp[n] = 0;
-
-  for (let start = n - 1; start >= 0; start--) {
-    dp[start] = dp[start + 1] + 1;
-    let node = root;
-    for (let end = start; end < n; end++) {
-      const char = s[end];
-      if (!(char in node.children)) {
-        break;
-      }
-      node = node.children[char];
-      if (node.isWord) {
-        dp[start] = Math.min(dp[start], dp[end + 1]);
-      }
-    }
-  }
-
-  return dp[0];
+  return dfs(0);
 };
