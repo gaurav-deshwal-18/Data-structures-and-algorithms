03876a940286ad4737f59103a7db307598a59f76
Gaurav Deshwal <gauravd@geekyants.com>
2024-05-09 12:09:07 +0530

    DSA

--- DSA/Problems/Data structures/Heap/Medium/Question 6/index.js
+++ DSA/Problems/Data structures/Heap/Medium/Question 6/index.js
@@ -1,1 +1,18 @@
-// Convert Min Heap To Max heap
+// K Closest Number
+var findClosestElements = function (arr, k, x) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (right - left + 1 > k) {
+    const calcLeft = Math.abs(arr[left] - x);
+    const calcRight = Math.abs(arr[right] - x);
+
+    if (calcLeft < calcRight || calcLeft === calcRight) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return arr.slice(left, right + 1);
+};
