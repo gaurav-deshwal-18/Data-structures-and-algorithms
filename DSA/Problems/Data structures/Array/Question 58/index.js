2fdcc79bacbddb59c3636411aee889490afa7b92
Gaurav Deshwal <gauravd@geekyants.com>
2024-05-13 15:38:14 +0530

    DSA

--- DSA/Problems/Data structures/Array/Question 58/index.js
+++ DSA/Problems/Data structures/Array/Question 58/index.js
@@ -1,13 +1,1 @@
-//*  Minimum Value to Get Positive Step by Step Sum
-
-var minStartValue = function (nums) {
-  let startValue = 1;
-  let sum = 0;
-  for (const num of nums) {
-    sum += num;
-    if (sum < 1) {
-      startValue = Math.max(1 - sum, startValue);
-    }
-  }
-  return startValue;
-};
+//*  Push Dominoes
